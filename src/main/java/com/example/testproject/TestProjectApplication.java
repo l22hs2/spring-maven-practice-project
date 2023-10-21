e35ec1640ec44cbd737128d74a975bf101a370d1
l22hs <l22hs2@gmail.com>
2023-10-21 16:29:41 +0900

    Controller 생성, @GetMapping

--- src/main/java/com/example/testproject/TestProjectApplication.java
+++ src/main/java/com/example/testproject/TestProjectApplication.java
@@ -3,11 +3,12 @@ package com.example.testproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class TestProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestProjectApplication.class, args);
-    }
 
+    }
 }
