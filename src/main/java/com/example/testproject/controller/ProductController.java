0024dc1fcb2e6e5da39e4f6b85749bca7fba4384
l22hs <l22hs2@gmail.com>
2023-10-27 13:47:32 +0900

    유효성 데이터 검증, Validation 어노테이션 적용 및 실습

--- src/main/java/com/example/testproject/controller/ProductController.java
+++ src/main/java/com/example/testproject/controller/ProductController.java
@@ -5,7 +5,11 @@ import com.example.testproject.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
@@ -35,15 +39,39 @@ public class ProductController {
 
     // http://localhost:8080/api/v1/product-api/product
     @PostMapping(value = "/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
+
+        // Validation Code Example
+        if (productDto.getProductId().equals("") || productDto.getProductId().isEmpty()) {
+            LOGGER.error("[createProduct] failed Response :: productId is Empty");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDto);
+        }
 
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
 
-        return productService.saveProduct(productId,productName,productPrice,productStock);
+        ProductDto response = productService
+                .saveProduct(productId, productName, productPrice, productStock);
+
+        LOGGER.info(
+                "[createProduct] Response >> productId : {}, productName : {}, productPrice : {}, productStock : {}",
+                response.getProductId(), response.getProductName(), response.getProductPrice(),
+                response.getProductStock());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+//    @PostMapping(value = "/product")
+//    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+//
+//        String productId = productDto.getProductId();
+//        String productName = productDto.getProductName();
+//        int productPrice = productDto.getProductPrice();
+//        int productStock = productDto.getProductStock();
+//
+//        return productService.saveProduct(productId,productName,productPrice,productStock);
+//    }
 
     // http://localhost:8080/api/v1/product-api/product/{productId}
     @DeleteMapping(value = "/product/{productId}")
--- src/main/java/com/example/testproject/data/dto/ProductDto.java
+++ src/main/java/com/example/testproject/data/dto/ProductDto.java
@@ -3,6 +3,8 @@ package com.example.testproject.data.dto;
 import com.example.testproject.data.entity.ProductEntity;
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,9 +13,21 @@ import lombok.*;
 
 // 계층간 데이터 교환, 전달
 public class ProductDto {
+    //@Size(min = 8, max = 8) # 글자 수 유효성
+    @NotNull
     private String productId;
+
+    @NotNull
     private String productName;
+
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 3000000)
     private int productPrice;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 9999)
     private int productStock;
 
     public ProductEntity toEntity(){
